e223263d38301ae5a0e1d255934080fcc2a509d6
Max Wennerfeldt <max.wennerfeldt@gmail.com>
2019-10-01 12:44:00 +0200

    Fixes

--- scripts/update_shairport_confs.sh
+++ scripts/update_shairport_confs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp /usr/share/audiohub/shairport-conf-local-template /usr/share/audiohub/shairport-conf-local
-cp /usr/share/audiohub/shairport-conf-remFront-template /usr/share/audiohub/shairport-remFront-local
-cp /usr/share/audiohub/shairport-conf-remRear-template /usr/share/audiohub/shairport-remRear-local
+cp /usr/share/audiohub/shairport-conf-remFront-template /usr/share/audiohub/shairport-conf-remFront
+cp /usr/share/audiohub/shairport-conf-remRear-template /usr/share/audiohub/shairport-conf-remRear
 sed -i -e s/DEV_NAME/"$1"/g /usr/share/audiohub/shairport-conf-local
 sed -i -e s/DEV_NAME/"$2"/g /usr/share/audiohub/shairport-conf-remFront
 sed -i -e s/DEV_NAME/"$3"/g /usr/share/audiohub/shairport-conf-remRear
